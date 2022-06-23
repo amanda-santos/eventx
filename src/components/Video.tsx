f2d5e31759ecb5aa7c5fcf20f76cfbd1ba681619
amanda-santos <amandasf0200@gmail.com>
2022-06-23 19:47:30 -0300

    feat: 💄 add video layout

--- src/components/Video.tsx
+++ src/components/Video.tsx
@@ -1,3 +1,114 @@
-export const Video = () => {
-  return <div className="flex-1">Video</div>;
+import {
+  CaretRight,
+  DiscordLogo,
+  FileArrowDown,
+  Lightning,
+} from "phosphor-react";
+import { ReactElement } from "react";
+
+export const Video = (): ReactElement => {
+  return (
+    <div className="flex-1">
+      <div className="bg-black flex justify-center">
+        <div className="h-full w-full max-w-[1100px] max-h-[60vh] aspect-video"></div>
+      </div>
+
+      <div className="p-8 max-w-[1100px] mx-auto">
+        <div className="flex items-start gap-16">
+          <div className="flex-1">
+            <h1 className="text-2xl font-bold">
+              Aula 1 - Abertura do Ignite Lab
+            </h1>
+            <p className="mt-4 text-gray-200 leading-relaxed">
+              Lorem ipsum odor amet, consectetuer adipiscing elit. Ut nullam
+              praesent accumsan mollis fermentum. Netus tristique vivamus
+              ullamcorper interdum natoque dignissim quam mattis nunc. Congue
+              mus integer consectetur sem vivamus magna ut gravida ullamcorper.
+              Elementum leo eget vel dui primis. Leo tempus auctor suscipit
+              class sodales maecenas conubia. Eros ac maximus litora tortor
+              class condimentum torquent. Iaculis aptent ipsum per auctor mus
+              vestibulum. Fusce consectetur senectus nam class tellus potenti
+              placerat. Ridiculus elit eleifend ornare hendrerit massa
+              vestibulum integer cubilia elementum.
+            </p>
+
+            <div className="flex items-center gap-4 mt-6">
+              <img
+                className="h-16 w-16 rounded-full border-2 border-blue-500"
+                src="https://github.com/amanda-santos.png"
+                alt=""
+              />
+
+              <div className="leading-relaxed">
+                <strong className="font-bold text-2xl block">
+                  Amanda Santos
+                </strong>
+                <span className="text-gray-200 text-sm block">
+                  Frontend Developer at Pipefy
+                </span>
+              </div>
+            </div>
+          </div>
+
+          <div className="flex flex-col gap-4">
+            <a
+              href=""
+              className="p-4 text-sm bg-green-500 flex items-center rounded font-bold uppercase gap-2 justify-center hover:bg-green-700 transition-colors"
+            >
+              <DiscordLogo size={24} />
+              Discord
+            </a>
+
+            <a
+              href=""
+              className="p-4 text-sm border border-blue-500 text-blue-500 flex items-center rounded font-bold uppercase gap-2 justify-center hover:bg-blue-500 hover:text-gray-900 transition-colors"
+            >
+              <Lightning size={24} />
+              Access Challenge
+            </a>
+          </div>
+        </div>
+
+        <div className="gap-8 mt-20 grid grid-cols-2">
+          <a
+            href=""
+            className="bg-gray-700 rounded overflow-hidden flex items-stretch gap-6 hover:bg-gray-600 transition-colors"
+          >
+            <div className="bg-green-700 h-full p-6 flex items-center">
+              <FileArrowDown size={40} />
+            </div>
+            <div className="py-6 leading-relaxed">
+              <strong className="text-2xl">Complementary Material</strong>
+              <p className="text-sm text-gray-200 mt-2">
+                Lorem ipsum odor amet, consectetuer adipiscing elit. Ut nullam
+                praesent accumsan mollis fermentum.
+              </p>
+            </div>
+            <div className="h-full p-6 flex items-center">
+              <CaretRight size={24} />
+            </div>
+          </a>
+
+          <a
+            href=""
+            className="bg-gray-700 rounded overflow-hidden flex items-stretch gap-6 hover:bg-gray-600 transition-colors"
+          >
+            <div className="bg-green-700 h-full p-6 flex items-center">
+              <FileArrowDown size={40} />
+            </div>
+            <div className="py-6 leading-relaxed">
+              <strong className="text-2xl">Wallpapers</strong>
+              <p className="text-sm text-gray-200 mt-2">
+                Lorem ipsum odor amet, consectetuer adipiscing elit. Ut nullam
+                praesent accumsan mollis fermentum.
+              </p>
+            </div>
+            <div className="h-full p-6 flex items-center">
+              <CaretRight size={24} />
+            </div>
+          </a>
+        </div>
+      </div>
+    </div>
+  );
 };
