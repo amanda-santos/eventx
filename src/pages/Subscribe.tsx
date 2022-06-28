aaa09747efee24f60cb0fb99a878cadbedcbc964
amanda-santos <amandasf0200@gmail.com>
2022-06-28 19:05:43 -0300

    feat: 💄 make subscription page responsive

--- src/pages/Subscribe.tsx
+++ src/pages/Subscribe.tsx
@@ -29,14 +29,14 @@ export const Subscribe = (): ReactElement => {
 
   return (
     <div className="min-h-screen bg-blur bg-cover bg-no-repeat flex flex-col items-center">
-      <div className="w-full max-w-[1100px] flex items-center justify-between mt-20 mx-auto">
-        <div className="max-w-[640px]">
+      <div className="w-full max-w-[1100px] flex flex-col lg:flex-row items-center justify-between mt-20 mx-auto">
+        <div className="max-w-[640px]  px-12">
           <Logo />
 
           <h1 className="mt-8 text-[2.5rem] leading-tight">
-            Build a{" "}
-            <strong className="text-pink-300">complete application</strong> from
-            scratch with <strong className="text-pink-300">React</strong>
+            The best platform <br />
+            to host your <br />
+            <strong className="text-pink-300">online events</strong>
           </h1>
           <p className="mt-4 text-gray-200 leading-relaxed">
             In just one week you will master in practice one of the most used
@@ -45,7 +45,7 @@ export const Subscribe = (): ReactElement => {
           </p>
         </div>
 
-        <div className="p-8 bg-gray-700 border border-gray-500 rounded">
+        <div className="p-8 bg-gray-700 lg:border lg:border-gray-500 rounded mt-12 w-full">
           <strong className="text-2xl mb-6 block">Subscribe for free</strong>
 
           <form onSubmit={handleSubmit} className="flex flex-col gap-2 w-full">
